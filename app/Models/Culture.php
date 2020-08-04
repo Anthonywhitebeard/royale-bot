18375fdf3daafc5336f72a635175053b3e1ecbac
fantik <fantom79022@gmail.com>
2020-08-04 14:08:05 +0300

    Add "culture" trait for bot, classes and events to get only "acceptable" and Active models on need

--- app/Models/Culture.php
+++ app/Models/Culture.php
@@ -4,7 +4,19 @@
 namespace App\Models;
 
 
+use Illuminate\Database\Eloquent\Builder;
+
 trait Culture
 {
+    /**
+     * @param Builder $builder
+     * @param Chat $chat
+     * @return Builder
+     */
+    public function scopeCulture(Builder $builder, Chat $chat)
+    {
+        return $builder->where('deviance', '<=', $chat->deviance)
+            ->where('active', 1);
+    }
 
 }
